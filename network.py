4daab47e018f4ea4531d0b63bd95dc65b4a28012
Lennart <lennart.bock@ipp.mpg.de>
2019-09-25 10:15:18 +0200

    just changed the generated data structure

--- network.py
+++ network.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 from matplotlib import pyplot as plt
+import numpy as np
 
 import time
 import random
@@ -35,59 +36,78 @@ class F_1(nn.Module):
         x = self.decoder(x)
         return x
 
-def generate_data(len1, len2):
+def generate_2Dtensor(len1, len2):
     data = []
     for _ in range(len2):
         list = [random.uniform(-1,1) for _ in range(len1)]
         data.append(list)
+    data = torch.from_numpy(data).float()
     dataset = torch.tensor(data)
     return dataset
 
 
-#TODO: prepare and load dataset as soon as I have it
-batch_size = 256
+def generate_vTBz(len, lenz):
+    vTB= np.random.rand(len,3)
+    z = np.random.rand(len,lenz)
+    vTBz = np.append(vTB,z,axis=1)
+    vTBz = torch.from_numpy(vTBz).float()
+    vTBz = torch.tensor(vTBz)
+    return vTBz
+if __name__ == '__main__':
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #TODO: prepare and load dataset as soon as I have it
+    batch_size = 256
 
-dataset = generate_data(200, 100)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)#creates iterable of dataset, shunks it in batch_sizes and shuffles the data
-
-
-
-
-w = 200 #width dimension of neural net
-d = 5 #depth of hidden layer
-tuple_length = 3 + len(z)
-
-model = F_1.to(device)
-criterion = nn.MSELoss()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    lenz = 150 #length of z coming from the magnetic configuration
+    dataset = generate_vTBz(10000,lenz)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)#creates iterable of dataset, shunks it in batch_sizes and shuffles the data
 
 
-learning_rate = 1e-3
-optimizer = torch.optim.Adam(
-    model.parameters(),
-    lr=learning_rate,
-)
 
-num_epochs = 50
-do = nn.Dropout()  # comment out for standard AE
-print("Starting training...")
-torch.cuda.empty_cache()
-total = 0
-for epoch in range(num_epochs):
-    start = time.clock()
-    for data in dataloader:
-        data= data.to(device)
-        data.requires_grad_()
 
-        # ===================forward=====================
-        output = model(data)
-        loss = criterion(output, data.data)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    # ===================log========================
-    end = time.clock() - start
-    total += end
-    print(f'epoch [{epoch + 1}/{num_epochs}], loss:{loss.item():.4f}, wall time:{end:.4f}, total time:{total:.4f}')
+    w = 200 #width dimension of neural net
+    d = 5 #depth of hidden layer
+    tuple_length = 3 + lenz
+
+    model = F_1().to(device)
+    criterion = nn.MSELoss()
+
+
+    learning_rate = 1e-3
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=learning_rate,
+    )
+    plt.ion()
+    num_epochs = 2000
+    do = nn.Dropout()  # comment out for standard AE
+    plot_loss = []
+    plot_epoch = []
+    print("Starting training...")
+    torch.cuda.empty_cache()
+    total = 0
+    for epoch in range(num_epochs):
+        start = time.clock()
+        for data in dataloader:
+            data= data.to(device)
+            data.requires_grad_()
+
+            # ===================forward=====================
+            output = model(data)
+            loss = criterion(output, data.data)
+            # ===================backward====================
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # ===================log========================
+        end = time.clock() - start
+        total += end
+        plot_loss.append(loss.item())
+        plot_epoch.append(epoch+1)
+        plt.scatter(plot_epoch,plot_loss)
+        print(f'epoch [{epoch + 1}/{num_epochs}], loss:{loss.item():.4f}, wall time:{end:.4f}, total time:{total:.4f}')
+    #print(data)
+    #print(output)
+    test = output-data
+    print(test)
